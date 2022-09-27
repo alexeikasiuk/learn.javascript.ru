9145a42dcfbebcf2dceb097acec16d5568486128
alexeikasiuk <aliakseikasiuk@gmail.com>
2022-09-27 17:58:37 +0300

    init: complete promise_basic task

---
+++ Promise/basic/task2.js
@@ -0,0 +1,39 @@
+/*Перепишите функцию showCircle, написанную в задании Анимация круга с помощью колбэка таким образом, чтобы она возвращала промис, вместо того чтобы принимать в аргументы функцию-callback.
+
+Новое использование:
+
+showCircle(150, 150, 100).then(div => {
+  div.classList.add('message-ball');
+  div.append("Hello, world!");
+});
+Возьмите решение из Анимация круга с помощью колбэка в качестве основы.
+*/
+
+function go() {
+  showCircle(150, 150, 100).then((div) => {
+    div.classList.add('message-ball');
+    div.append('hello, world!');
+  });
+}
+
+function showCircle(cx, cy, radius) {
+  let div = document.createElement('div');
+  div.style.width = 0;
+  div.style.height = 0;
+  div.style.left = cx + 'px';
+  div.style.top = cy + 'px';
+  div.className = 'circle';
+  document.body.append(div);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      div.style.width = radius * 2 + 'px';
+      div.style.height = radius * 2 + 'px';
+
+      div.addEventListener('transitionend', function handler() {
+        console.log('transitioned');
+        div.removeEventListener('transitionend', handler);
+        resolve(div);
+      });
+    }, 0);
+  });
+}
